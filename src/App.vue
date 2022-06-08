b4b6a73d73caa21fcfad25f82ba9c43b763f7023
Andrea Guidotti <andrea19912014@gmail.com>
2022-06-08 16:28:58 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myHeader from './components/myHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myHeader
   }
 }
 </script>
---
+++ src/components/myHeader.vue
@@ -0,0 +1,98 @@
+<template>
+    <header>
+        <img src="../assets/dc-logo.png" alt="dc_logo_img">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TEST
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name : 'myHeader',
+    }
+</script>
+
+<style scoped lang="scss">
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+        height: 140px;
+        // debug
+        background: grey;
+
+        img {
+            max-width: 100%;
+            height: 100px;
+        }
+
+        ul {
+            display: flex;
+            justify-content: space-evenly;
+            list-style-type: none;
+
+            li {
+                padding: 60px 20px;
+
+                a {
+                    text-decoration: none;
+                    color: #404a5c;
+                }
+            }
+        }
+    }
+</style>
